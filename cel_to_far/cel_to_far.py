36a9429e61d32f249a39022b9b274f9c15ab5d7b
Filip Jukić <no.idea13@gmail.com>
2011-04-15 00:48:30 +0200

    Accidentally added older version of code

--- cel_to_far/cel_to_far.py
+++ cel_to_far/cel_to_far.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import sys
 
 
 def convert(celsius):
@@ -10,3 +13,11 @@ def convert(celsius):
         raise TypeError
     fahrenheit = celsius * (9 / 5.) + 32
     return fahrenheit
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print "Usage: ./cel_to_far.py temp_in_cel"
+    else:
+        celsius = sys.argv[1]
+        fahrenheit = convert(celsius)
+        print "%s°C = %.2f°F" % (celsius, fahrenheit)
